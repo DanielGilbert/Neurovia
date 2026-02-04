5672eab49eb5dc16dec5dae881dc7a4bb3af4db0
Daniel Gilbert <ich@danielgilbert.de>
2026-02-04 17:06:43 +0100

    Re-Arrange everything

---
+++ src/main.c
@@ -0,0 +1,19 @@
+﻿#include "raylib.h"
+
+int main(void)
+{
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+
+    InitWindow(1920,1080,"Neurovia");
+
+    while (!WindowShouldClose()) {
+        ClearBackground(BLACK);
+        BeginDrawing();
+        DrawFPS(10,10);
+        EndDrawing();
+    }
+
+    CloseWindow();          // Close window and OpenGL context
+
+    return 0;
+}
