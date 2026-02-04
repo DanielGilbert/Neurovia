5626bb1d9ee2b15787bf66649a13cae4054bdc65
Daniel Gilbert <ich@danielgilbert.de>
2026-02-04 20:48:45 +0100

    Initialize skeleton

--- src/main.c
+++ src/main.c
@@ -1,19 +1,52 @@
 ﻿#include "raylib.h"
+#include "raymath.h"
+
+const int screenHeight = 1080;
+const int screenWidth = 1920;
+
+void GameStartup();
+void GameUpdate();
+void GameRender();
+void GameShutdown();
 
 int main(void)
 {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
 
-    InitWindow(1920,1080,"Neurovia");
+    GameStartup();
+
+    InitWindow(screenWidth, screenHeight, "Neurovia");
 
     while (!WindowShouldClose()) {
+        
+        GameUpdate();
         ClearBackground(BLACK);
         BeginDrawing();
         DrawFPS(10,10);
+        DrawText("This is a test!", 10, 30, 22, PINK);
+        GameRender();
         EndDrawing();
     }
+
+    GameShutdown();
 
     CloseWindow();          // Close window and OpenGL context
 
     return 0;
+}
+
+void GameStartup() {
+    InitAudioDevice();
+}
+
+void GameUpdate() {
+
+}
+
+void GameRender() {
+
+}
+
+void GameShutdown() {
+    CloseAudioDevice();
 }
